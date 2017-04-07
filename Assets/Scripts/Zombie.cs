6d9fc41f86029bd8dd44c6a875eae972506ee8af
emerson reyes <emersonreyes727@gmail.com>
2017-04-07 14:09:48 -0700

    Create zombie script

---
+++ Assets/Scripts/Zombie.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Zombie : MonoBehaviour {
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+}
