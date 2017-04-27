716efb9e3ca5a25766951b8c6051fa138ef1473d
emerson reyes <emersonreyes727@gmail.com>
2017-04-27 14:01:17 -0700

    Add background music, zombie attack sound fx

--- Assets/Scripts/ZombieController.cs
+++ Assets/Scripts/ZombieController.cs
@@ -8,11 +8,12 @@ public class ZombieController : MonoBehaviour {
 
 	[SerializeField] private Transform playerPosition;
 	[SerializeField] private float range = 3.0f;
-	[SerializeField] private float timeBetweenAttacks = 1.0f;
+	[SerializeField] private AudioClip attackSfx;
 
 	private Animator anim;
 	private NavMeshAgent navMeshAgent;
-	private bool playerInRange;
+
+	private AudioSource audio;
 
 	void Awake () {
 		Assert.IsNotNull (playerPosition);
@@ -22,28 +23,18 @@ public class ZombieController : MonoBehaviour {
 	void Start () {
 		anim = GetComponent<Animator> ();
 		navMeshAgent = GetComponent<NavMeshAgent> ();
-
-		StartCoroutine (attack());
+		audio = GetComponent<AudioSource> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (Vector3.Distance(transform.position, playerPosition.position) < range) {
-			playerInRange = true;
-		} else {
-			playerInRange = false;
+			audio.PlayOneShot (attackSfx);
+			anim.Play ("attack");
 
+		} else {
 			anim.Play ("walk");
 			navMeshAgent.SetDestination (playerPosition.position);
 		}
-	}
-
-	IEnumerator attack () {
-		if (playerInRange && !GameManager.instance.GameOver) {
-			anim.Play ("attack");
-			yield return new WaitForSeconds (timeBetweenAttacks);
-		}
-		yield return null;
-		StartCoroutine (attack());
 	}
 }
